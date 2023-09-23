faac1157c74caf4fe9c81eb554d995272a9282d8
Arash-11 <arashnawy@gmail.com>
2023-09-22 21:07:13 -0400

    Add generate-ast script

---
+++ src/Expr.ts
@@ -0,0 +1,66 @@
+import Token from './token';
+
+interface ExprVisitor<R> {
+  visitBinaryExpr(expr: Binary): R;
+  visitGroupingExpr(expr: Grouping): R;
+  visitLiteralExpr(expr: Literal): R;
+  visitUnaryExpr(expr: Unary): R;
+}
+
+interface Expr {
+  accept<R>(visitor: ExprVisitor<R>): R;
+}
+
+class Binary implements Expr {
+  left: Expr;
+  operator: Token;
+  right: Expr;
+
+  constructor(left: Expr, operator: Token, right: Expr) {
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<R>(visitor: ExprVisitor<R>): R {
+    return visitor.visitBinaryExpr(this);
+  }
+}
+
+class Grouping implements Expr {
+  expression: Expr;
+
+  constructor(expression: Expr) {
+    this.expression = expression;
+  }
+
+  accept<R>(visitor: ExprVisitor<R>): R {
+    return visitor.visitGroupingExpr(this);
+  }
+}
+
+class Literal implements Expr {
+  value: object;
+
+  constructor(value: object) {
+    this.value = value;
+  }
+
+  accept<R>(visitor: ExprVisitor<R>): R {
+    return visitor.visitLiteralExpr(this);
+  }
+}
+
+class Unary implements Expr {
+  operator: Token;
+  right: Expr;
+
+  constructor(operator: Token, right: Expr) {
+    this.operator = operator;
+    this.right = right;
+  }
+
+  accept<R>(visitor: ExprVisitor<R>): R {
+    return visitor.visitUnaryExpr(this);
+  }
+}
---
+++ src/utils/GenerateAst.ts
@@ -0,0 +1,93 @@
+import fs from 'fs';
+import path from 'path';
+
+class GenerateAst {
+  static main() {
+    const args = process.argv.slice(2);
+
+    if (args.length !== 1) {
+      console.error("Usage: generate-ast <output directory>");
+      process.exit(64);
+    }
+
+    const outputDir = args[0];
+    this.defineAst(outputDir, 'Expr', [
+      'Binary   - left: Expr, operator: Token, right: Expr',
+      'Grouping - expression: Expr',
+      'Literal  - value: object',
+      'Unary    - operator: Token, right: Expr'
+    ]);
+  }
+
+  private static defineAst(outputDir: string, baseName: string, types: string[]) {
+    const filePath = path.join(outputDir, `${baseName}.ts`);
+
+    // Replace content if file already exists
+    fs.writeFileSync(filePath, "import Token from './token';\n");
+
+    this.defineVisitor(filePath, baseName, types);
+
+    this.defineAcceptMethod(filePath, baseName);
+
+    // The AST classes
+    for (const type of types) {
+      const className = type.split('-')[0].trim();
+      const fields = type.split('-')[1].trim();
+      this.defineType(filePath, baseName, className, fields);
+    }
+  }
+
+  private static defineVisitor(filePath: string, baseName: string, types: string[]) {
+    let fileContent = `\ninterface ${baseName}Visitor<R> {\n`;
+
+    for (const type of types) {
+      const typeName = type.split('-')[0].trim();
+      fileContent += `  visit${typeName}${baseName}(${baseName.toLowerCase()}: ${typeName}): R;\n`;
+    }
+
+    fileContent += '}\n';
+
+    fs.appendFileSync(filePath, fileContent);
+  }
+
+  private static defineAcceptMethod(filePath: string, baseName: string) {
+    let fileContent = `\ninterface ${baseName} {\n`;
+    fileContent    += `  accept<R>(visitor: ${baseName}Visitor<R>): R;\n`;
+    fileContent    += '}\n';
+
+    fs.appendFileSync(filePath, fileContent);
+  }
+
+  private static defineType(filePath: string, baseName: string, className: string, fieldList: string) {
+    let fileContent = `\nclass ${className} implements ${baseName} {\n`
+
+    // Properties
+    const fields = fieldList.split(', ');
+    for (const field of fields) {
+      fileContent += `  ${field};\n`;
+    }
+
+    fileContent += '\n';
+
+    // Constructor
+    fileContent += `  constructor(${fieldList}) {\n`;
+
+    // Store parameters in fields
+    for (const field of fields) {
+      const name = field.split(':')[0];
+      fileContent += `    this.${name} = ${name};\n`;
+    }
+
+    fileContent += '  }\n';
+
+    fileContent += `\n  accept<R>(visitor: ${baseName}Visitor<R>): R {\n`;
+    fileContent += `    return visitor.visit${className}${baseName}(this);\n`;
+    fileContent += '  }\n';
+
+    fileContent += '}\n';
+
+    fs.appendFileSync(filePath, fileContent);
+  }
+}
+
+GenerateAst.main();
